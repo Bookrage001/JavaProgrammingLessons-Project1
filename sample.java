5be7fcc17ab4229dfe0a91a74c1e7b3974195edf
ArdernC <caleb.ardern@assertiv.com>
2020-03-29 13:41:31 +1100

    inital commit

---
+++ sample.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class main here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class sample
+{
+    public static String sampleText = "Sample Text";
+
+    public static void main(String args[]){
+        
+        System.out.println("This is the: System.out.println");
+        System.out.println(sampleText);
+        System.out.println(sampleText + " " + sampleText);
+
+        
+    }
+}
